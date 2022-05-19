b2e175534b32800467c9375f3a3adaeb57f3f2b1
Damola Olutoke <olutokeezekiel18@gmail.com>
2022-05-19 17:19:24 +0100

    Update Routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -12,6 +12,7 @@ import Signup from '../pages/Signup'
 import SingleForum from '../pages/SingleForum'
 import TutorSignup from '../pages/TutorSignup'
 import Users from '../pages/Users'
+import FacilitatorSignup from '../pages/FacilitatorSignup'
 
 const PagesRoutes = () => {
     const route = document.location.pathname
@@ -21,10 +22,11 @@ const PagesRoutes = () => {
                 <Route path='/' element={<Home />} /> 
                 <Route path='/signup' element={<Signup />} /> 
                 <Route path='/signup/faculty' element={<TutorSignup />} /> 
+                <Route path= '/signup/facilitator' element={<FacilitatorSignup/>} />
                 <Route path='/login' element={<Login />} /> 
                 <Route path='/call_for_application' element={<CFA />} /> 
                 <Route path='/users' element={<Users />} /> 
-                <Route path='/cohort' element={<Cohorts />} /> 
+                <Route path='/information' element={<Cohorts />} /> 
                 <Route path='/assignments' element={<Assignments />} /> 
                 <Route path='/forums' element={<Forums />} /> 
                 <Route path='/forums/:id' element={<SingleForum />} /> 
